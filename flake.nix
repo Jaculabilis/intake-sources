56c52c38ab18b0124a9609af903470cedc680059
Tim Van Baak <tim.vanbaak@gmail.com>
2023-06-10 17:01:20 -0700

    Add package config for building intake-rss

--- flake.nix
+++ flake.nix
@@ -8,6 +8,15 @@
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
   in {
+    packages.${system} = {
+      intake-rss = pkgs.python38Packages.buildPythonPackage {
+        name = "intake-rss";
+        src = builtins.path { path = ./intake-rss; name = "intake-rss"; };
+        format = "pyproject";
+        propagatedBuildInputs = with pkgs.python38Packages; [ feedparser setuptools ];
+      };
+    };
+
     devShells.${system} = {
       intake-rss = let
         pythonEnv = pkgs.python38.withPackages (pypkgs: with pypkgs; [ black feedparser ]);
